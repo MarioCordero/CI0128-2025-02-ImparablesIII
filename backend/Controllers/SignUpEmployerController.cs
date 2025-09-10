983f0f516513de9b3d8f565db5b046aa41e0123b
Diego Bolanos <diego.bolanosvillalobos@ucr.ac.cr>
2025-09-10 01:20:47 -0700

    Added SignUpEmployer Controller

---
+++ backend/Controllers/SignUpEmployerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_c28730.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpEmployerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "SignUpEmployerController is working!";
+        }
+    }
+}
