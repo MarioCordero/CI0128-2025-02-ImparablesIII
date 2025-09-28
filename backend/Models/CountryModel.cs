2c6e30d46d9abcd9e27e6ea1d6d58b3fa3be2f06
czuniga28 <chrisz.28052003@gmail.com>
2025-09-28 16:53:42 -0600

    REFRACT: delete unused files

--- backend/Models/CountryModel.cs
+++
@@ -1,9 +0,0 @@
-namespace backend_lab_c28730.Models;
-
-public class CountryModel
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Continent { get; set; }
-    public string Language { get; set; }
-}
