269c06af198074be42cbdfe8c19741d19aa7af8f
czuniga28 <chrisz.28052003@gmail.com>
2025-10-29 12:07:40 -0600

    FEAT: Añadir campos de deducciones y salario neto en la tabla DetallePlanilla

--- DatabaseScripts/11_DetallePlanilla.sql
+++ DatabaseScripts/11_DetallePlanilla.sql
@@ -5,7 +5,10 @@ CREATE TABLE PlaniFy.DetallePlanilla (
     idEmpleado INT,
     idPlanilla INT,
     salarioBruto INT,
+    DeduccionesEmpleado INT,
+    DeduccionesEmpresa INT,
     totalBeneficios INT,
+    salarioNeto INT,
     PRIMARY KEY (idEmpleado, idPlanilla),
     FOREIGN KEY (idEmpleado) REFERENCES PlaniFy.Empleado(idPersona),
     FOREIGN KEY (idPlanilla) REFERENCES PlaniFy.Planilla(id)
