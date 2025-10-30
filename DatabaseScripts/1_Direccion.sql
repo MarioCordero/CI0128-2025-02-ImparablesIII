f0109de904c7e59ce3eeddf0bb78977159be4144
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2025-10-29 18:02:20 -0600

    FEAT/REFRACT: Arreglando SQL

--- DatabaseScripts/1_Direccion.sql
+++ DatabaseScripts/1_Direccion.sql
@@ -3,10 +3,10 @@
 -- ===================================
 CREATE TABLE PlaniFy.Direccion (
     id INT IDENTITY PRIMARY KEY NOT NULL,
-    Provincia NVARCHAR(12),
-    Canton NVARCHAR(30),
-    Distrito NVARCHAR(30),
-    DireccionParticular NVARCHAR(150),
+    Provincia NVARCHAR(24), -- CORREGIDO: 12 → 24
+    Canton NVARCHAR(60),    -- CORREGIDO: 30 → 60
+    Distrito NVARCHAR(60),  -- CORREGIDO: 30 → 60
+    DireccionParticular NVARCHAR(300), -- CORREGIDO: 150 → 300
     CONSTRAINT CK_Direccion_Provincia CHECK (
         Provincia IN (
             N'San José',
--- DatabaseScripts/8_Beneficio.sql
+++ DatabaseScripts/8_Beneficio.sql
@@ -3,12 +3,12 @@
 -- ===================================
 CREATE TABLE PlaniFy.Beneficio (
     idEmpresa INT NOT NULL,
-    Nombre NVARCHAR(50) NOT NULL,
-    TipoCalculo NVARCHAR(20) NOT NULL,
-    Tipo NVARCHAR(20) NOT NULL,
+    Nombre NVARCHAR(100) NOT NULL,      -- CORREGIDO: 20 → 100
+    TipoCalculo NVARCHAR(40) NOT NULL,  -- CORREGIDO: 20 → 40
+    Tipo NVARCHAR(40) NOT NULL,         -- CORREGIDO: 20 → 40
     Valor INT,
     Porcentaje INT,
+    Descripcion VARCHAR(200),           -- AÑADIDO: Faltaba
     PRIMARY KEY (idEmpresa, Nombre),
-    FOREIGN KEY (idEmpresa) REFERENCES PlaniFy.Empresa(Id)
-        ON DELETE CASCADE
+    FOREIGN KEY (idEmpresa) REFERENCES PlaniFy.Empresa(Id) ON DELETE CASCADE
 );
