88c9f0c801ce8961aa5fde247231e9494bc99e4c
MarioCordero <mariogabriel.cordero@uacr.ac.cr>
2025-11-22 10:54:48 -0600

    REFRACT: - Cambiando EmailExtensions

--- backend/Extensions/EmailExtensions.cs
+++ backend/Extensions/EmailExtensions.cs
@@ -9,18 +9,14 @@ namespace backend.Extensions
     {
         public static async Task<bool> SendEmailAsync(this IServiceProvider serviceProvider, string receiverEmail, string subject, string body, bool isHtml = false)
         {
-            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-            var emailSettings = new EmailSettings();
-            configuration.GetSection("EmailSettings").Bind(emailSettings);
-            return await EmailHelper.SendEmailAsync(receiverEmail, subject, body, emailSettings, isHtml);
+            var emailHelper = serviceProvider.GetRequiredService<IEmailHelper>();
+            return await emailHelper.SendEmailAsync(receiverEmail, subject, body, isHtml);
         }
 
         public static async Task<EmailResponseDto> SendEmailWithResponseAsync(this IServiceProvider serviceProvider, string receiverEmail, string subject, string body, bool isHtml = false)
         {
-            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-            var emailSettings = new EmailSettings();
-            configuration.GetSection("EmailSettings").Bind(emailSettings);            
-            return await EmailHelper.SendEmailWithResponseAsync(receiverEmail, subject, body, emailSettings, isHtml);
+            var emailHelper = serviceProvider.GetRequiredService<IEmailHelper>();           
+            return await emailHelper.SendEmailWithResponseAsync(receiverEmail, subject, body, isHtml);
         }
     }
 }
