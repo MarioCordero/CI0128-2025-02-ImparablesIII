27752fa38436f1ad419d03a0a15293164733f985
MarioCordero <mariogabriel.cordero@uacr.ac.cr>
2025-11-22 00:21:34 -0600

    REFRACT: - Refactorizando Persona repository, para seguir principios SOLID

--- backend/Repositories/IPersonaRepository.cs
+++ backend/Repositories/IPersonaRepository.cs
@@ -1,10 +1,11 @@
 using backend.DTOs;
+using backend.Models;
 
 namespace backend.Repositories
 {
     public interface IPersonaRepository
     {
-        Task<int> CreatePersonaAsync(string email, string nombre, string? segundoNombre, string apellidos, DateTime fechaNacimiento, string cedula, string rol, int? telefono, int direccionId);
+        Task<int> CreatePersonaAsync(Persona persona);
         Task<bool> IsEmailAvailableAsync(string email);
         Task<bool> IsCedulaAvailableAsync(string cedula);
         Task<bool> TestConnectionAsync();
